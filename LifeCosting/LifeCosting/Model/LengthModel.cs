865d7076ece6e95ce4d76ebc9dcd9b49b79813e8
Christie Beukman <me.mcbeukman@gmail.com>
2017-06-18 09:22:21 +0200

    Temp

---
+++ LifeCosting/LifeCosting/Model/LengthModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LifeCosting.Model.DataLayer;
+
+namespace LifeCosting.Model
+{
+    public class LengthModel
+    {
+        
+        public int LengthCostID { get; set; }
+        public int TypeID { get; set; }
+        public string MaterialID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal? NoPieces { get; set; }
+        public decimal? LengthPerPiece { get; set; }
+        public decimal? TotalLength { get; set; }
+        public decimal? TotalCostPieces { get; set; }
+        public decimal? PricePerMeter { get; set; }
+
+    }
+}
---
+++ LifeCosting/LifeCosting/ViewModels/LengthCostingViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apex.MVVM;
+using LifeCosting.Model;
+using LifeCosting.Model.DataLayer;
+
+namespace LifeCosting.ViewModels
+{
+   public class LengthCostingViewModel : ViewModel
+    {
+        dbNewEntities ctx = new dbNewEntities();    // DataModel Entities datacontext
+
+        private List<LengthModel> _LengthModels;
+        private LengthModel _SelectedLengthModel;
+
+
+        public List<LengthModel> LengthModel
+        {
+            get
+            {
+                return _LengthModels;
+            }
+
+            set
+            {
+                _LengthModels = value;
+                NotifyPropertyChanged("LengthModel");
+            }
+        }
+
+        public LengthModel SelectedLengthModel
+        {
+            get
+            {
+                return _SelectedLengthModel;
+            }
+
+            set
+            {
+                _SelectedLengthModel = value;
+                NotifyPropertyChanged("SelectedLengthModel");
+            }
+        }
+
+
+        #region COnstructor
+
+        public LengthCostingViewModel()
+        {
+            GetLenght();
+        }
+
+        #endregion
+
+        private void GetLenght()
+        {
+            var Query = (from l in ctx.tblLengthCosts
+                         join m in ctx.tblMaterials
+                         on l.MaterialID equals m.MaterialID
+                         select new LengthModel
+                         {
+                             MaterialID = l.MaterialID,
+                             Name = m.Name,
+                             Description = m.Description,
+                             NoPieces = l.NoPieces,
+                             LengthPerPiece = l.LengthPerPiece,
+                             TotalCostPieces = l.TotalCostPieces,
+                             TotalLength = l.TotalLength,
+                             PricePerMeter = l.PricePerMeter
+                         }).ToList();
+            this.LengthModel = Query;
+        }
+    }
+}
